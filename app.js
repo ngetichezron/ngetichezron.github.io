fad7fcd467a17bfb0e808868f7e1653c409fa781
ngetichezron <ngetichezron4@gmail.com>
2023-05-01 18:38:34 +0300

    Updated CV

--- app.js
+++ app.js
@@ -13,66 +13,113 @@
 })();
 
 
-const backArrow = document.getElementById('back-arrow');
-
-// Add an event listener to the window object to detect when the user navigates to a new page
-window.addEventListener('popstate', (event) => {
-// If there is a previous history state, navigate back to that page
-if (event.state) {
-    location.href = event.state.pageUrl;
-}
-});
-
-backArrow.addEventListener('click', () => {
-// Go back to the previous history state
-history.back();
-});
-
-
-
-
-
-
-
-
-
-
-const form = document.querySelector('#contact-form');
-const submitButton = document.querySelector('#submit-btn');
-    
-form.addEventListener('submit', (event) => {
-    event.preventDefault(); // Prevent form submission
-    
-    // Get form data
-    const name = document.querySelector('#name').value;
-    const email = document.querySelector('#email').value;
-    const subject = document.querySelector('#subject').value;
-    const message = document.querySelector('#message').value;
-    
-    // Send form data using fetch API //(url of actual server side script)
-    fetch('server.js', {
+const submitBtn = document.getElementById('submit-btn');
+submitBtn.addEventListener('click', (event) => {
+  event.preventDefault(); // prevent form submission
+  
+  const nameInput = document.getElementById('name');
+  const emailInput = document.getElementById('email');
+  const subjectInput = document.getElementById('subject');
+  const messageInput = document.getElementById('message');
+  
+  if (!nameInput.value || !emailInput.value || !subjectInput.value || !messageInput.value) {
+    alert('Please fill in all fields.'); // display error message
+    return;
+  }
+  
+  fetch('/submit-form', {
     method: 'POST',
-    headers: {
-        'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({ name, email, subject, message })
-    })
-    .then(response => response.json())
-    .then(data => {
-    if (data.success) {
-        alert('Your message has been sent successfully!');
-        form.reset();
+    body: new FormData(document.getElementById('contact-form'))
+  })
+  .then((response) => {
+    if (response.ok) {
+      alert('Form submitted successfully.');
+    } else if (response.status === 405) {
+      alert('Something Went wrong. Please try again later.');
     } else {
-        alert('Something went wrong. Please try again later.');
+      alert('An error occured. Please try again later.');
     }
-    })
-    .catch(error => {
+  })
+  .catch((error) => {
     console.error(error);
-    alert('Something went wrong. Please try again later.');
-    })
-    .finally(() => {
-    submitButton.disabled = false; // Enable submit button
-    });
-    
-    submitButton.disabled = true; // Disable submit button to prevent multiple submissions
+    alert('An error occurred. Please try again later.');
+  });
 });
+
+
+
+
+
+// const submitBtn = document.getElementById('submit-btn');
+// submitBtn.addEventListener('click', (event) => {
+//   const nameInput = document.getElementById('name');
+//   const emailInput = document.getElementById('email');
+//   const subjectInput = document.getElementById('subject');
+//   const messageInput = document.getElementById('message');
+  
+//   if (!nameInput.value || !emailInput.value || !subjectInput.value || !messageInput.value) {
+//     event.preventDefault(); // prevent form submission
+//     alert('Please fill in all fields.'); // display error message
+//   }
+//   else {
+//     alert('Something went wrong...')
+//   }
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const form = document.querySelector('#contact-form');
+// const submitButton = document.querySelector('#submit-btn');
+    
+// form.addEventListener('submit', (event) => {
+//     event.preventDefault(); // Prevent form submission
+    
+//     // Get form data
+//     const name = document.querySelector('#name').value;
+//     const email = document.querySelector('#email').value;
+//     const subject = document.querySelector('#subject').value;
+//     const message = document.querySelector('#message').value;
+    
+//     // Send form data using fetch API //(url of actual server side script)
+//     fetch('server.js', {
+//     method: 'POST',
+//     headers: {
+//         'Content-Type': 'application/json'
+//     },
+//     body: JSON.stringify({ name, email, subject, message })
+//     })
+//     .then(response => response.json())
+//     .then(data => {
+//     if (data.success) {
+//         alert('Your message has been sent successfully!');
+//         form.reset();
+//     } else {
+//         alert('Something went wrong. Please try again later.');
+//     }
+//     })
+//     .catch(error => {
+//     console.error(error);
+//     alert('Something went wrong. Please try again later.');
+//     })
+//     .finally(() => {
+//     submitButton.disabled = false; // Enable submit button
+//     });
+    
+//     submitButton.disabled = true; // Disable submit button to prevent multiple submissions
+// });
